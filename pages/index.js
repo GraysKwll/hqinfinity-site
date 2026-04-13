66b81b19ee2cc580237b0d3052ff4e3888b2b360
GraysKwll <ramosabraao11@gmail.com>
2026-04-12 21:57:05 -0300

    Add HQInfinity landing page React component

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HQInfinityLanding = () => {
+    return (
+        <div>
+            <h1>Welcome to HQ Infinity</h1>
+            <p>Your ultimate solution for productivity and efficiency!</p>
+        </div>
+    );
+};
+
+export default HQInfinityLanding;
